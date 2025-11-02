17b60d10ead14ab989c026d1be1464939aab91bc
Casanovacarlos <125916735+Casanovacarlos@users.noreply.github.com>
2025-11-02 11:49:09 -0500

    Add personaje routes to index routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,8 @@
 import ejemplo from './ejemplo.routes.js';
 import { Router } from 'express';
-
+import personaje from './personaje.routes.js';
 const indexRoutes = Router();
 
 indexRoutes.use('/ejemplo', ejemplo);
-
+indexRoutes.use('/personajes', personaje);
 export default indexRoutes;
