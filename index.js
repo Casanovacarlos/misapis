1ea4ab2787480e494c90b78fb3df1921fc24945a
Casanovacarlos <125916735+Casanovacarlos@users.noreply.github.com>
2025-11-02 11:51:50 -0500

    Import personajeRoutes in index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Server from './server/server.js';
 import colors from 'colors';
 import dotenv from 'dotenv';
+import personajeRoutes from './routes/personaje.routes.js';
 dotenv.config();
 
 const server = new Server();
